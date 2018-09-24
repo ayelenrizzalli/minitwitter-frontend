90c8226a1f4bbc5f8fafd0b4c875c678d2279715
Ayelen <ayelenrizzalli@gmail.com>
2018-09-24 15:43:53 -0300

    First React Router nav

--- src/app.js
+++ src/app.js
@@ -6,16 +6,31 @@ import UsersList from './users/components/userslist.js';
 import LoginContainer from './login/containers/LoginContainer.js';
 import Feed from './Feed/containers/FeedContainer.js';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 const app = document.getElementById('app');
 render(
-  <BrowserRouter>
-    <Fragment>
-      <LoginContainer />
-      <Route exact path="/users/" component={UsersList}/>
-    </Fragment>
-  </BrowserRouter>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/feed">Feed</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={LoginContainer} />
+          <Route path="/users" component={UsersList} />
+          <Route path="/feed" component={Feed} />
+        </div>
+      </Router>
   ,app
 );
