b022130c7bc8275e2f92c7878c3a191676d5cc37
Ayelen <ayelenrizzalli@gmail.com>
2018-09-24 16:56:36 -0300

    Get user Feed after login

--- src/Tweets/containers/TweetsContainer.js
+++ src/Tweets/containers/TweetsContainer.js
@@ -12,10 +12,30 @@ class TweetsContainer extends Component {
     };
 
   componentDidMount() {
-      
-      axios.get('http://localhost:3001/feed')
-        .then(json => { console.log(json.data); this.setState({tweets: json.data.feed}) })
-  };
+    const scope = this;
+    let reqData={
+      "x-auth-token" : axios.defaults.headers.common['x-auth-token']
+    }
+    axios({
+          method: 'get',
+          url: 'http://localhost:3001/feed',
+          withCredentials: true,
+          crossdomain: true,
+          data: Object.keys(reqData).map(function(key) {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(reqData[key])}).join('&'),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      }
+    }).then(function (response) {
+        console.log("with auth");
+        console.log(response);
+        console.log(typeof(response));
+        scope.setState({ tweets : response.data.feed });
+    })
+    .catch(function (error) {
+      console.log("Get Error : " + error);
+    });
+  }
 
   render() {
     return (
